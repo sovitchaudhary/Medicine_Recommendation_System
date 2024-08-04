e6080ae0e040f5d0fc8b7c178fe447d1b597c10c
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-08-05 01:59:19 +0530

    Added Routes

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
+# creating routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/developer')
+def developer():
+    return render_template('developer.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 
 
